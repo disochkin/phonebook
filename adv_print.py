f4b917b19cb60c28efa81beacf625e72df8175e7
Denis Nagovitsyn <disochkin@gmail.com>
2020-01-28 22:19:25 +0500

    fix error

--- adv_print.py
+++ adv_print.py
@@ -10,21 +10,16 @@ def adv_print(*args, **kwargs):
     if in_file:
         with open(output_file, 'w') as f_obj:
             if start:
-                if in_file:
-                    print(start, sep='', file=f_obj)
-                for item in args:
-                    print('\n'.join(textwrap.wrap(item, max_line)), file=f_obj)
+                print(start, sep='', file=f_obj)
+            for item in args:
+                print('\n'.join(textwrap.wrap(item, max_line)), file=f_obj)
 
     else:
         if start:
-            if in_file:
-                print(start, sep='')
+            print(start, sep='')
             for item in args:
                 print('\n'.join(textwrap.wrap(item, max_line)))
 
 
 adv_print("zzzzzzzzzzzzzzzzzzzzz","zzzzxxxxxxxxxxxxxxxxxxxxxxxxx")
-adv_print("zzzzzzzzzzzzzzzzzzzzz","zzzzxxxxxxxxxxxxxxxxxxxxxxxxx", max_line=8, start='start_line!')
-adv_print("zzzzzzzzzzzzzzzzzzzzz","zzzzxxxxxxxxxxxxxxxxxxxxxxxxx", max_line=8, start='start_line!', in_file=True)
 
-#print("zzz","zzzz")
